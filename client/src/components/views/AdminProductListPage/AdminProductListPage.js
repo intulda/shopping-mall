9b0102b80e70d986a322ca655f620397b8f8378d
geonwan <psog03081@naver.com>
2020-09-05 13:08:10 +0900

    test

--- client/src/components/views/AdminProductListPage/AdminProductListPage.js
+++ client/src/components/views/AdminProductListPage/AdminProductListPage.js
@@ -34,7 +34,7 @@ function AdminProductListPage(){
 
 const gridMain = AdminProductInfo.map((product, index) => {
     return <Row key={index}>
-        <Col span={4} order={5} onClick>
+        <Col span={4} order={5}>
             <a href>{product.title}</a>
         </Col>
         <Col span={4} order={4} >
